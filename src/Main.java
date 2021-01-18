ffda0b25ac1a7df4451e30c63a69663d38763451
Alejandro <alejandro@corco.technology>
2021-01-18 19:06:30 +0100

    Class Main has been created. Package main and utils have been created. Enum Color and class Logger have been creaated.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/*
+ * @function: Aquesta classe és la classe Main, és a dir la que executarà.
+ * @author: Alejandro Córcoles
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/utils/Color.java
@@ -0,0 +1,36 @@
+package utils;
+
+/*
+ * @function: Aquest enum conté tots els colors necessaris per a formatar el programa
+ * @author: Alejandro Córcoles
+ */
+
+public enum Color {
+    // Resetear color
+    RESET("\033[0m"),
+
+    // Colores en negrita
+    BLACK_BOLD("\033[1;30m"),   // Negre
+    RED_BOLD("\033[1;31m"),     // Vermell
+    GREEN_BOLD("\033[1;32m"),   // Verd
+    YELLOW_BOLD("\033[1;33m"),  // Groc
+    BLUE_BOLD("\033[1;34m"),    // Blau
+    MAGENTA_BOLD("\033[1;35m"), // Magenta
+    CYAN_BOLD("\033[1;36m"),    // Cyan
+    WHITE_BOLD("\033[1;37m");   // Blanc
+
+
+    private final String code;
+
+
+    // Constructor del enum
+    Color(String code) {
+        this.code = code;
+    }
+
+    @Override
+    // El override s'utilitza per substituir la funció toString per la que vull possar. En aquest cas vull que em retorni la variable.
+    public String toString() {
+        return code;
+    }
+}
